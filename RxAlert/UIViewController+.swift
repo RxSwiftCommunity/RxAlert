9a23e1ac8ac45fd8e436d49f411831b4ebe3dcfb
Asaf Baibekov <asafall21@gmail.com>
2020-01-22 17:10:04 +0200

    cleanup alert method

--- RxAlert/UIViewController+.swift
+++ RxAlert/UIViewController+.swift
@@ -45,16 +45,16 @@ extension UIAlertController {
 // MARK: - UIViewController
 extension UIViewController {
     public func alert(title: String?,
-               message: String? = nil,
-               actions: [AlertAction],
-               preferredStyle:UIAlertController.Style = .alert,
-               vc:UIViewController) -> Observable<Int> {
-        
-        let actionSheet = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
-        
-        return actionSheet.addAction(actions: actions)
-            .do(onSubscribed: {
-                vc.present(actionSheet, animated: true, completion: nil)
-            })
+                      message: String? = nil,
+                      actions: [AlertAction],
+                      preferredStyle: UIAlertController.Style = .alert,
+                      vc: UIViewController) -> Observable<Int> {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        return
+            alertController
+                .addAction(actions: actions)
+                .do(onSubscribed: {
+                    vc.present(alertController, animated: true)
+                })
     }
 }
