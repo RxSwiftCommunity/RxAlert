cf12040e529296f7803c4d821c0aaa0a26fc126f
keisukeYamagishi <jam330257@gmail.com>
2020-02-27 20:43:02 +0900

    added UITextField to RxAlert

--- RxAlert/AlertAction.swift
+++ RxAlert/AlertAction.swift
@@ -30,13 +30,21 @@ public struct AlertAction {
     
     public let title: String
     public let type: Int
+    public let textField: UITextField?
     public let style: UIAlertAction.Style
     
-    public init(title: String,
+    public init(title: String = "",
                 type: Int = 0,
+                textField: UITextField? = nil,
+                placeholder: String? = nil,
                 style: UIAlertAction.Style = .default) {
         self.title = title
         self.type = type
+        self.textField = textField
+        if self.textField != nil
+            && placeholder != nil {
+            self.textField?.placeholder = placeholder
+        }
         self.style = style
     }
 }
---
+++ RxAlert/OutputAction.swift
@@ -0,0 +1,14 @@
+//
+//  OutputAction.swift
+//  RxAlert
+//
+//  Created by Shichimitoucarashi on 2020/02/27.
+//  Copyright © 2020 Shichimitoucarashi. All rights reserved.
+//
+
+import Foundation
+
+public struct OutputAction {
+    public var index: Int
+    public var textFields: [UITextField]?
+}
